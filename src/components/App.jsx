1376b317d642518cbce476250f8d5b7a2a29e8f7
snmirji1997 <snmirji1997@gmail.com>
2022-10-24 00:12:11 +0530

    Component Structure & Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import {CssBaseline} from '@mui/material';
+import {Route,Switch} from 'react-router-dom';
+import {Actors, Movies, MoviesInformation, NavBar, Profile} from './index'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <CssBaseline/>
+    <NavBar/>
+     <main>
+      <Switch>
+        <Route exact path="/movies/:id">
+        <MoviesInformation/>
+        </Route>
+        <Route exact path="/actors/:id">
+          <Actors/>
+        </Route>
+        <Route exact path="/">
+        <Movies/>
+        </Route>
+        <Route exact path="/profile/:id">
+          <Profile/>
+        </Route>
+      </Switch>
+     </main>
+    </div>
   )
 }
 
---
+++ src/components/MoviesInformation/MoviesInformation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoviesInformation = () => {
+  return (
+    <div>MoviesInformation</div>
+  )
+}
+
+export default MoviesInformation
