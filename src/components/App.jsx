2c5d73dd9c347d1e7999d0f4bda30975fd2b9fc5
snmirji1997 <snmirji1997@gmail.com>
2022-10-24 12:37:38 +0530

    Setup Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {CssBaseline} from '@mui/material';
 import {Route,Switch} from 'react-router-dom';
-import {Actors, Movies, MoviesInformation, NavBar, Profile} from './index'
+import {Actors, Movies, MoviesInformation, NavBar, Profile} from './index';
+import useStyles from './styles';
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
     <CssBaseline/>
     <NavBar/>
-     <main>
+     <main className={classes.content}>
+     <div className={classes.toolbar}/>
       <Switch>
         <Route exact path="/movies/:id">
         <MoviesInformation/>
